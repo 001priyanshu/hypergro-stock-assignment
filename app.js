d8b2f3dc474f0f1f9f25ad4fecaecdaffa1b0590
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-23 19:40:30 +0530

    sxript added

--- app.js
+++ app.js
@@ -2,15 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 require('dotenv').config();
+const { runDataProcessingBeforeServerStarts } = require('./dataProcessing');
+const stocksRouter = require('./routes/stocks');
+
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use('/stocks', stocksRouter);
+
 
 mongoose.connect(process.env.DATABASE_URL).then(async () => {
   console.log('Connected to the database');
 
+  await runDataProcessingBeforeServerStarts();
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
