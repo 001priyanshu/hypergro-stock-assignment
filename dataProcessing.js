1823ac5bdd78343c269adb06f3fc6ccc6b441a06
Priyanshu Rajput <priyanshurajput822400@gmail.com>
2024-01-23 23:01:57 +0530

    bugs

--- dataProcessing.js
+++ dataProcessing.js
@@ -6,101 +6,107 @@ const Stock = require("./models/stockModel");
 const csv = require('csv-parser');
 
 async function fetchDataForDays(days) {
-    const currentDate = new Date();
-    const datePromises = [];
-  
-    for (let i = days-1; i >= 0; i--) {
-      const currentDateCopy = new Date(currentDate);
-      currentDateCopy.setDate(currentDateCopy.getDate() - i);
-  
-      const formattedDate = currentDateCopy.toLocaleDateString('en-GB', {
-        year: '2-digit',
-        month: '2-digit',
-        day: '2-digit',
-      }).replace(/\//g, '');
-  
-      const latestEntry = await Stock.findOne().sort({ createdAt: -1 });
+  const currentDate = new Date();
+  const datePromises = [];
+
+  for (let i = days - 1; i >= 0; i--) {
+    const currentDateCopy = new Date(currentDate);
+    currentDateCopy.setDate(currentDateCopy.getDate() - i);
+
+    const formattedDate = currentDateCopy.toLocaleDateString('en-GB', {
+      year: '2-digit',
+      month: '2-digit',
+      day: '2-digit',
+    }).replace(/\//g, '');
+
+    const latestEntry = await Stock.findOne().sort({ createdAt: -1 });
+
+    if (latestEntry) {
+      latestEntryDate = new Date(latestEntry.createdAt);
       
-  
-      const latestEntryDate = new Date(latestEntry.createdAt);
-      latestEntryDate.setHours(0, 0, 0, 0); 
-      
-      const currentDateCopyMidnight = new Date(currentDateCopy);
-      currentDateCopyMidnight.setHours(0, 0, 0, 0); 
-      
-      if (latestEntryDate >= currentDateCopyMidnight) {
-        console.log(`Data for ${formattedDate} already exists. Skipping data fetch and processing.`);
-        continue;
-      }
-     
-  
-      const BSE_URL = `https://www.bseindia.com/download/BhavCopy/Equity/EQ${formattedDate}_CSV.ZIP`;
-      const DOWNLOAD_PATH = `downloads/data_${formattedDate}.zip`;
-      const EXTRACT_PATH = 'downloads/';
-      const csvHeaders = ['SC_CODE', 'SC_NAME', 'SC_GROUP', 'SC_TYPE', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'LAST', 'PREVCLOSE', 'NO_TRADES', 'NO_OF_SHRS', 'NET_TURNOV', 'TDCLOINDI'];
-  
-      const promise =await downloadData(BSE_URL, DOWNLOAD_PATH)
-        .then(() => extractData(DOWNLOAD_PATH, EXTRACT_PATH))
-        .then(() => {
-          const data = [];
-          const filePath = `${EXTRACT_PATH}EQ${formattedDate}.CSV`;
-  
-          return new Promise((resolve, reject) => {
-            let isFirstRow = true;
-  
-            const stream = fs.createReadStream(filePath)
-              .pipe(csv({ headers: csvHeaders }))
-              .on('data', (row) => {
-                if (isFirstRow) {
-                  for (const originalColumn in row) {
-                    const index = csvHeaders.indexOf(originalColumn);
-                    if (index !== -1) {
-                      const newColumn = csvHeaders[index];
-                      row[newColumn] = originalColumn;
-                      delete row[originalColumn];
-                    }
-                  }
-                  isFirstRow = false;
-                }
-  
-                const stockData = {
-                  code: row['SC_CODE'],
-                  name: row['SC_NAME'],
-                  open: parseFloat(row['OPEN']) || 0,
-                  high: parseFloat(row['HIGH']) || 0,
-                  low: parseFloat(row['LOW']) || 0,
-                  close: parseFloat(row['CLOSE']) || 0,
-                  createdAt:currentDateCopy,
-                };
-  
-                if (!isNaN(stockData.open) && !isNaN(stockData.high) && !isNaN(stockData.low) && !isNaN(stockData.close) && !isNaN(stockData.code)) {
-                  data.push(stockData);
-                }
-              })
-              .on('end', () => {
-                resolve(data);
-              })
-              .on('error', (error) => {
-                reject(error);
-              });
-  
-            stream.resume();
-          });
-        })
-        .then((data) => {
-          return saveToDatabase(data);
-        })
-        .then(() => {
-          console.log(`Data for ${formattedDate} saved to the database successfully.`);
-        })
-        .catch((error) => {
-          console.error(`Error during data processing for ${formattedDate}:`);
-        });
-  
-      datePromises.push(promise);
+    } else {
+      const currentDate = new Date();
+      currentDate.setDate(currentDate.getDate() - 51);
+      latestEntryDate = currentDate;
     }
-  
-    await Promise.all(datePromises);
+    latestEntryDate.setHours(0, 0, 0, 0);
+
+    const currentDateCopyMidnight = new Date(currentDateCopy);
+    currentDateCopyMidnight.setHours(0, 0, 0, 0);
+
+    if (latestEntryDate >= currentDateCopyMidnight) {
+      console.log(`Data for ${formattedDate} already exists. Skipping data fetch and processing.`);
+      continue;
+    }
+
+
+    const BSE_URL = `https://www.bseindia.com/download/BhavCopy/Equity/EQ${formattedDate}_CSV.ZIP`;
+    const DOWNLOAD_PATH = `downloads/data_${formattedDate}.zip`;
+    const EXTRACT_PATH = 'downloads/';
+    const csvHeaders = ['SC_CODE', 'SC_NAME', 'SC_GROUP', 'SC_TYPE', 'OPEN', 'HIGH', 'LOW', 'CLOSE', 'LAST', 'PREVCLOSE', 'NO_TRADES', 'NO_OF_SHRS', 'NET_TURNOV', 'TDCLOINDI'];
+
+    const promise = await downloadData(BSE_URL, DOWNLOAD_PATH)
+      .then(() => extractData(DOWNLOAD_PATH, EXTRACT_PATH))
+      .then(() => {
+        const data = [];
+        const filePath = `${EXTRACT_PATH}EQ${formattedDate}.CSV`;
+
+        return new Promise((resolve, reject) => {
+          let isFirstRow = true;
+
+          const stream = fs.createReadStream(filePath)
+            .pipe(csv({ headers: csvHeaders }))
+            .on('data', (row) => {
+              if (isFirstRow) {
+                for (const originalColumn in row) {
+                  const index = csvHeaders.indexOf(originalColumn);
+                  if (index !== -1) {
+                    const newColumn = csvHeaders[index];
+                    row[newColumn] = originalColumn;
+                    delete row[originalColumn];
+                  }
+                }
+                isFirstRow = false;
+              }
+
+              const stockData = {
+                code: row['SC_CODE'],
+                name: row['SC_NAME'],
+                open: parseFloat(row['OPEN']) || 0,
+                high: parseFloat(row['HIGH']) || 0,
+                low: parseFloat(row['LOW']) || 0,
+                close: parseFloat(row['CLOSE']) || 0,
+                createdAt: currentDateCopy,
+              };
+
+              if (!isNaN(stockData.open) && !isNaN(stockData.high) && !isNaN(stockData.low) && !isNaN(stockData.close) && !isNaN(stockData.code)) {
+                data.push(stockData);
+              }
+            })
+            .on('end', () => {
+              resolve(data);
+            })
+            .on('error', (error) => {
+              reject(error);
+            });
+
+          stream.resume();
+        });
+      })
+      .then((data) => {
+        return saveToDatabase(data);
+      })
+      .then(() => {
+        console.log(`Data for ${formattedDate} saved to the database successfully.`);
+      })
+      .catch((error) => {
+        console.error(`Error during data processing for ${formattedDate}:`);
+      });
+
+    datePromises.push(promise);
+  }
+
+  await Promise.all(datePromises);
 }
 
 async function runDataProcessingBeforeServerStarts() {
--- routes/stocks.js
+++ routes/stocks.js
@@ -6,103 +6,126 @@ const mongoose = require('mongoose');
 
 
 router.get('/top10', async (req, res) => {
-    console.log("JJJ")
-try {
+
+  try {
     const topStocks = await Stock.find().limit(10).sort({ close: -1 });
     res.json(topStocks);
-} catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
-}
+  }
 });
 
 router.get('/search', async (req, res) => {
-    const { name } =  req.query;
-    console.log(name,"**");
+  const { name } = req.query;
+  console.log(name, "**");
 
-if (!name) {
+  if (!name) {
     return res.status(400).json({ error: 'Name parameter is required' });
-}
+  }
 
-try {
+  try {
     const foundStocks = await Stock.find({ name: { $regex: new RegExp(name, 'i') } });
     res.json(foundStocks);
-} catch (error) {
+  } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
-}
+  }
 });
 router.post('/favorites', async (req, res) => {
-    const { stockId } = req.body;
-   console.log(stockId);
-    if (!stockId) {
-      return res.status(400).json({ error: 'Stock ID is required' });
-    }
-  
-    try {
-      const newFavorite = new Favorite({ stock: stockId });
-      await newFavorite.save();
-  
-      await Stock.findByIdAndUpdate(stockId, { $push: { favorites: newFavorite._id } });
-  
-      res.status(201).json(newFavorite);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  const { stockId } = req.body;
+  console.log(stockId);
+  if (!stockId) {
+    return res.status(400).json({ error: 'Stock ID is required' });
+  }
 
-  router.get('/favorites', async (req, res) => {
-    try {
-      const favoriteStocks = await Favorite.find().populate('stock');
-      res.json(favoriteStocks);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-  
-  router.delete('/favorites/:id', async (req, res) => {
-    const { id } = req.params;
-  
-    if (!id) {
-      return res.status(400).json({ error: 'ID parameter is required' });
-    }
-  
-    try {
-      const favorite = await Favorite.findByIdAndDelete(id);
-      await Stock.findByIdAndUpdate(favorite.stock, { $pull: { favorites: favorite._id } });
-  
-      res.json({ message: 'Stock removed from favorites' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const newFavorite = new Favorite({ stock: stockId });
+    await newFavorite.save();
 
-  router.get('/stockHistory/:code', async (req, res) => {
-    const { code } = req.params;
-  
-    if (!code) {
-      return res.status(400).json({ error: 'Code parameter is required' });
-    }
-  
-    try {
-      const currentDate = new Date();
-      const fiftyDaysAgo = new Date();
-      fiftyDaysAgo.setDate(fiftyDaysAgo.getDate() - 50);
-     console.log(currentDate)
-     console.log(fiftyDaysAgo)
-      const stockData = await Stock.find({
-        code,
-        createdAt: { $gte: fiftyDaysAgo, $lte: currentDate },
-      }).sort({ createdAt: -1 });
-  
-      res.json(stockData);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+    await Stock.findByIdAndUpdate(stockId, { $push: { favorites: newFavorite._id } });
 
-  module.exports = router;
+    res.status(201).json(newFavorite);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/favorites', async (req, res) => {
+  try {
+    const favoriteStocks = await Favorite.find().populate('stock');
+    res.json(favoriteStocks);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.delete('/favorites/:id', async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+
+  if (!id) {
+    return res.status(400).json({ error: 'ID parameter is required' });
+  }
+
+  try {
+    const favorite = await Favorite.findByIdAndDelete(id);
+    if(!favorite){
+     return res.json({message:'Stock not available'})
+    }
+    await Stock.findByIdAndUpdate(favorite.stock, { $pull: { favorites: favorite._id } });
+
+    res.json({ message: 'Stock removed from favorites' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/stockHistory/:code', async (req, res) => {
+  const { code } = req.params;
+
+  if (!code) {
+    return res.status(400).json({ error: 'Code parameter is required' });
+  }
+
+  try {
+    const currentDate = new Date();
+    const fiftyDaysAgo = new Date();
+    fiftyDaysAgo.setDate(fiftyDaysAgo.getDate() - 50);
+    console.log(currentDate)
+    console.log(fiftyDaysAgo)
+    const stockData = await Stock.find({
+      code,
+      createdAt: { $gte: fiftyDaysAgo, $lte: currentDate },
+    }).sort({ createdAt: -1 });
+
+    res.json(stockData);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+router.get('/stocksByDate/:date', async (req, res) => {
+  const requestedDate = new Date(req.params.date);
+
+  try {
+    const stocksOnDate = await Stock.find({
+      createdAt: {
+        $gte: requestedDate,
+        $lt: new Date(requestedDate.getTime() + 24 * 60 * 60 * 1000),
+      },
+    });
+
+    res.json(stocksOnDate);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+module.exports = router;
+
